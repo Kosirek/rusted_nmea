54076ad116ad539bd2493bdd98048f7b0fb555ce
Michal Kosairz <michal.kosiarz@gmail.com>
2024-12-30 08:09:01 +0100

    Add trait implementation for messagers
    Mainly for message serialization and deserialization.
    
    Signed-off-by: Michal Kosairz <michal.kosiarz@gmail.com>

--- src/devices.rs
+++ src/devices.rs
@@ -1,9 +1,12 @@
 pub trait NmeaBuff {
+    /// Used for NMEA message serialization from bottom to top structures
     fn as_string(&self) -> String;
 
+    /// Used for parsing (deserialization) NMEA message from top to bottom structures
     fn parse_payload(msg: String) -> Self;
 }
 
+#[allow(dead_code)]
 enum DeviceId {
     None,
     GPS,
@@ -23,11 +26,13 @@ impl DeviceId {
     }    
 }
 
+#[allow(dead_code)]
 struct NmeaMessage<T> {
     device_id: DeviceId,
     message_body: T,
 }
 
+#[allow(dead_code)]
 impl<T> NmeaMessage<T>
 where
     T: NmeaBuff,
@@ -39,13 +44,22 @@ where
         }
     }
 
-    // pub fn empty(payload: T) -> NmeaMessage<T> {
+    pub fn empty(payload: T) -> NmeaMessage<T> {
+        NmeaMessage {
+            device_id: DeviceId::None,
+            message_body: payload,
+        }
+    }
 
-    // }
+    fn checksum(msg: String) -> u8 {
+        let sum: u32 = msg.into_bytes().iter().map(|x| *x as u32).sum();
+        sum as u8
+    }
 
     pub fn create_message(&self) -> String {
         let payload = self.message_body.as_string();
-        let message = format!("${}{}*{:#02x}\r", self.device_id.id_string().unwrap(), payload, 12);
+        let message_body = format!("${}{}*", self.device_id.id_string().unwrap(), payload);
+        let message = format!("{}{:#02x}\r\n", message_body.clone(), NmeaMessage::<T>::checksum(message_body)); 
         message
     }
 
